a29f4141f63a24d38ccc3bc20a41c97c1b204960
chishige1217200 <daichicchi01@gmail.com>
2025-10-05 22:29:32 +0900

    Add; トースト通知機能を追加

--- components/main.tsx
+++ components/main.tsx
@@ -1,4 +1,5 @@
 // App.tsx
+import { Slide, toast, ToastContainer } from "react-toastify";
 import Chat from "./chat";
 import Sidebar from "./sidebar";
 
@@ -8,9 +9,23 @@ type MainProps = {
 
 export default function Main({ paramSessionId }: MainProps) {
   console.log("paramSessionId:", paramSessionId);
+  toast.success("paramSessionId: " + paramSessionId);
 
   return (
     <div className="flex h-screen">
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Slide}
+      />
       <Sidebar />
       <Chat />
     </div>
